d88569cfc194cf7290407f61873ae3c433f897a4
jhosua.tsakana <jhosua.tsakana@gmail.com>
2022-08-08 08:11:40 -0500

    add: methods to get and put entries

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,82 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import mongoose from "mongoose";
+import { db } from "../../../database";
+import { Entry, IEntry } from "../../../models";
+
+type Data = { message: string } | IEntry;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { id } = req.query;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ message: `Invalid ID ${id}` });
+  }
+
+  switch (req.method) {
+    case "GET":
+      return getEntry(req, res);
+    case "PUT":
+      return updateEntry(req, res);
+    default:
+      return res.status(400).json({ message: "Method not exist" });
+  }
+}
+
+const getEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query;
+
+  await db.connect();
+
+  const entryInDB = await Entry.findById(id);
+  await db.disconnect();
+
+  if (!entryInDB) {
+    return res.status(400).json({ message: `Entry not found with id:${id}` });
+  }
+
+  return res.status(200).json(entryInDB);
+};
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query;
+
+  await db.connect();
+
+  const entryToUpdate = await Entry.findById(id);
+
+  if (!entryToUpdate) {
+    await db.disconnect();
+    return res.status(400).json({ message: `Entry not found with id: ${id}` });
+  }
+
+  const {
+    description = entryToUpdate.description,
+    status = entryToUpdate.status,
+  } = req.body;
+
+  try {
+    const updatedEntry = await Entry.findByIdAndUpdate(
+      id,
+      {
+        description,
+        status,
+      },
+      {
+        new: true,
+        runvalidators: true,
+      }
+    );
+
+    await db.disconnect();
+
+    return res.status(200).json(updatedEntry!);
+  } catch (error: any) {
+    console.log({ error });
+
+    await db.disconnect();
+    res.status(400).json({ message: "Something went wrong..." });
+  }
+};
