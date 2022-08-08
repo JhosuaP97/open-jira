a63042ef7741f03ffe6bc800508253d2423d2306
jhosua.tsakana <jhosua.tsakana@gmail.com>
2022-08-08 14:11:06 -0500

    add: snackbar to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,23 @@ import type { AppProps } from "next/app";
 
 import { UIProvider } from "../context/ui";
 import { EntriesProvider } from "../context/entries";
+import { SnackbarProvider } from "notistack";
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { darkTheme, lightTheme } from "../themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <EntriesProvider>
-      <UIProvider>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </UIProvider>
-    </EntriesProvider>
+    <SnackbarProvider maxSnack={3}>
+      <EntriesProvider>
+        <UIProvider>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </UIProvider>
+      </EntriesProvider>
+    </SnackbarProvider>
   );
 }
 
