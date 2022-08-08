137c40f3c3856e107c19407839dcf1986668c1d2
jhosua.tsakana <jhosua.tsakana@gmail.com>
2022-08-08 14:10:27 -0500

    add: EntryPage

---
+++ pages/entries/[id].tsx
@@ -0,0 +1,166 @@
+import { ChangeEvent, FC, useMemo, useState, useContext } from "react";
+import { GetServerSideProps } from "next";
+
+import {
+  capitalize,
+  Card,
+  CardContent,
+  TextField,
+  CardHeader,
+  Grid,
+  CardActions,
+  Button,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  IconButton,
+} from "@mui/material";
+import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+
+import { Layout } from "../../components/layouts/Layout";
+import { Entry, EntryStatus } from "../../interfaces";
+import { dbEntries } from "../../database";
+import { EntriesContext } from "../../context/entries";
+import { useRouter } from "next/router";
+
+const validStatus: EntryStatus[] = ["pending", "in-progress", "finished"];
+
+interface Props {
+  entry: Entry;
+}
+
+const EntryPage: FC<Props> = ({ entry }) => {
+  const { updatedEntry, deleteEntry } = useContext(EntriesContext);
+  const router = useRouter();
+
+  const [inputValue, setInputValue] = useState(entry.description);
+  const [status, setStatus] = useState<EntryStatus>(entry.status);
+  const [touched, setTouched] = useState(false);
+
+  const onInputValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const onStatusChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    setStatus(event.target.value as EntryStatus);
+  };
+
+  const onSave = () => {
+    if (inputValue.trim().length === 0) return;
+
+    const updateEntry: Entry = {
+      ...entry,
+      description: inputValue,
+      status,
+    };
+    updatedEntry(updateEntry, true);
+    setTouched(false);
+  };
+
+  const isNotValid = useMemo(
+    () => inputValue.length <= 0 && touched,
+    [inputValue, touched]
+  );
+
+  const onDelete = () => {
+    deleteEntry(entry._id);
+    router.push("/");
+  };
+
+  return (
+    <Layout title={inputValue.substring(0, 20) + "..."}>
+      <Grid container justifyContent="center" sx={{ marginTop: 2 }}>
+        <Grid item xs={12} sm={8} md={6}>
+          <Card>
+            <CardHeader
+              title={`Entrada:`}
+              subheader={`Creada hace: ${entry.createdAt} minutos`}
+            />
+
+            <CardContent>
+              <TextField
+                sx={{ marginTop: 2, marginBottom: 1 }}
+                fullWidth
+                placeholder="Nueva entrada"
+                autoFocus
+                multiline
+                label="Nueva entrada"
+                value={inputValue}
+                onChange={onInputValueChange}
+                onBlur={() => setTouched(true)}
+                helperText={isNotValid && "Ingrese un valor"}
+                error={isNotValid}
+              />
+
+              <FormControl>
+                <FormLabel>Estado</FormLabel>
+                <RadioGroup row value={status} onChange={onStatusChanged}>
+                  {validStatus.map((option) => (
+                    <FormControlLabel
+                      key={option}
+                      value={option}
+                      control={<Radio />}
+                      label={capitalize(option)}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+
+              <CardActions>
+                <Button
+                  startIcon={<SaveOutlinedIcon />}
+                  variant="contained"
+                  fullWidth
+                  onClick={onSave}
+                  disabled={inputValue.length <= 0}
+                >
+                  Save
+                </Button>
+              </CardActions>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+
+      <IconButton
+        onClick={onDelete}
+        sx={{
+          position: "fixed",
+          bottom: 30,
+          right: 30,
+          backgroundColor: "error.dark",
+        }}
+      >
+        <DeleteOutlinedIcon />
+      </IconButton>
+    </Layout>
+  );
+};
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { id } = params as { id: string };
+
+  const entry = await dbEntries.getEntryById(id);
+
+  if (!entry) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      entry,
+    },
+  };
+};
+
+export default EntryPage;
